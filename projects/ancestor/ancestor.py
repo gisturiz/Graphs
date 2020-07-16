8271c867e4eb4e53af857d57b4cc426ced1e1263
Gustavo Isturiz <isturizgustavo@gmail.com>
2020-07-15 21:38:05 -0400

    ancestor update

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -82,4 +82,4 @@ def earliest_ancestor(ancestors, starting_node):
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
                   (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-print(earliest_ancestor(test_ancestors, 7))
+print(earliest_ancestor(test_ancestors, 1))
